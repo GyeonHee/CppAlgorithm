fede739feaa4b39af62599ee75683a0f68f14a29
GyeonHee <104518499+GyeonHee@users.noreply.github.com>
2025-10-28 17:40:53 +0900

    [Silver IV] Title: 동전 0, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.cc
@@ -0,0 +1,29 @@
+#include<iostream>
+
+using namespace std;
+
+int N, K;
+int ans = 0;
+int arr[15];
+
+int main() 
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	
+	cin >> N >> K;
+
+	for (int i = 0; i < N; ++i)
+	{
+		cin >> arr[i];
+	}
+	for (int i = N - 1; i >= 0; --i)
+	{
+		ans +=  K / arr[i];
+		K %= arr[i];
+	}
+	
+	cout << ans;
+
+	return 0;
+}
